06346dbfe67eee68d54a76ca5d919dedcb504231
Javier Gualpa <javier_gualpa@hotmail.com>
2023-02-18 18:57:45 -0500

    Updates

--- assets/Img.3632135a.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index.b543cc9f.js";const e=()=>t("div",{className:"text-black text-2xl",children:"Soy una Gran Imagen"});export{e as Img};
---
+++ assets/Img.a2f00b75.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.218a9d3a.js";const e=()=>t("div",{className:"text-black text-2xl",children:"Soy una Gran Imagen"});export{e as Img};
